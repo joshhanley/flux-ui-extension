0956659bfbf6c36e6d30bc0aedf3c05c13e7bab0
Josh Hanley <josh@hitsystems.com.au>
2024-08-29 18:24:47 +1000

    Update page details

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-const FluxCompletionProvider = require('./src/flux-completion-provider')
+const FluxCompletionProvider = require('./flux-completion-provider')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
